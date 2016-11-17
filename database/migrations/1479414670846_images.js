bb1de819071bc278d20918cebe987eaa4995985c
Alex Windle <alexwindle85@gmail.com>
2016-11-17 15:35:12 -0500

    Add new migration.

---
+++ database/migrations/1479414670846_images.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class ImagesTableSchema extends Schema {
+
+  up () {
+    this.create('images', (table) => {
+      table.increments()
+      table.timestamps()
+      table.string('url')
+      table.string('description')
+      table.string('likeCount')
+    })
+  }
+
+  down () {
+    this.drop('images')
+  }
+
+}
+
+module.exports = ImagesTableSchema
